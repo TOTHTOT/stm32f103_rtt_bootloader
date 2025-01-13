bffbdd1f402b0e0dee77ade36937469199df5628
TOTHTOT <37585883+TOTHTOT@users.noreply.github.com>
2025-01-13 09:19:38 +0800

    删除objects文件

--- Objects/cctype.d
+++
@@ -1,3 +0,0 @@
-.\objects\cctype.o: ..\..\..\components\libc\compilers\common\cctype.c
-.\objects\cctype.o: ..\..\..\components\libc\compilers\common\include\posix/ctype.h
-.\objects\cctype.o: D:\WorkSoft\ARM\ARMCC\Bin\..\include\ctype.h
--- Objects/cwchar.d
+++
@@ -1,4 +0,0 @@
-.\objects\cwchar.o: ..\..\..\components\libc\compilers\common\cwchar.c
-.\objects\cwchar.o: ..\..\..\components\libc\compilers\common\include\posix/wchar.h
-.\objects\cwchar.o: D:\WorkSoft\ARM\ARMCC\Bin\..\include\wchar.h
-.\objects\cwchar.o: D:\WorkSoft\ARM\ARMCC\Bin\..\include\string.h
